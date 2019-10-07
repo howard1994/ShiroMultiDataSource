ef5fb7be106897cb02b858ea9a2247afeae8da55
howard1994 <565449577@qq.com>
2019-10-07 17:24:27 +0800

    初始化

---
+++ src/main/java/com/bitcoding/template/howard/HowardApplication.java
@@ -0,0 +1,13 @@
+package com.bitcoding.template.howard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowardApplication.class, args);
+    }
+
+}
